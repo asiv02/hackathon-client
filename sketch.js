6b969f7b9cf0e2f4902f8d0c83ed5a9303784dfc
Angie Siv <siva@emmanuel.edu>
2024-01-17 14:29:19 -0500

    added more images

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-function setup () {
-  
+//Quad Map
+let img; // Declare variable 'img'.
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  img = loadImage('quad_map.jpg'); // Load the image
 }
 
-function draw () {
-  
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays geage at point (0, height/2) at half size
+  //image(img, 0, height / 2, img.width / 2, img.height / 2);
 }
